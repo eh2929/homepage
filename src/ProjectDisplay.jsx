c11d3b004a5dfbac0e06403ce330405e2705022b
Ethan <ethanhough29@gmail.com>
2024-05-13 17:46:08 -0400

    added github urls

--- src/ProjectDisplay.jsx
+++ src/ProjectDisplay.jsx
@@ -23,34 +23,40 @@ function ProjectDisplay() {
       title: "Lend Ease",
       description:
         "Lend Ease is a communications solution for indepedent third party lenders and their clients.",
+      url: "https://github.com/eh2929/Mortgage-Client-Communications",
     },
     {
       image: image2,
       title: "Job Trackr",
       description:
         "An app to help you track and visualize your job search progress.",
+      url: "https://github.com/eh2929/Job-app-tracker",
     },
     {
       image: image3,
       title: "Crate Joy",
       description:
         "An e-commerce marketplace for vendors to sell subscription boxes.",
+      url: "https://github.com/eh2929/SubscriptionBox-Marketplace",
     },
     {
       image: image4,
       title: "myPoke Collector",
       description: "A Pokemon card collection tracker.",
+      url: "https://github.com/eh2929/Phase1-Final",
     },
     {
       image: image5,
       title: "CLI Job Tracker",
       description:
         "A command line interface to help you track your job search progress.",
+      url: "https://github.com/eh2929/Job-Application-Tracker",
     },
     {
       image: image6,
       title: "Let's Find Games",
       description: "A platform for gamers to find games based on their mood.",
+      url: "https://github.com/eh2929/phase-2-project",
     },
     // Add more cards as needed
   ];
@@ -58,7 +64,7 @@ function ProjectDisplay() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
       {cards.map((card, index) => (
-        <div key={index} className="max-w-xs mx-auto rounded-xl m-4">
+        <div key={index} className="max-w-xs mx-auto rounded-xl m-2">
           <Card>
             <CardHeader>
               <CardTitle>{card.title}</CardTitle>
@@ -77,10 +83,11 @@ function ProjectDisplay() {
               <Button className="bg-slate-600 dark:bg-slate-100 px-4 py-2">
                 DEMO
               </Button>
-
-              <Button className="bg-slate-600 dark:bg-slate-100 px-4 py-2 ml-2">
-                GitHub
-              </Button>
+              <a href={card.url} target="_blank" rel="noopener noreferrer">
+                <Button className="bg-slate-600 dark:bg-slate-100 px-4 py-2 ml-2">
+                  GitHub
+                </Button>
+              </a>
             </CardFooter>
           </Card>
         </div>
